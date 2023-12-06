9cb40a7bbcc4968e61b2a8bdc96579edcdad8ded
ussenuk <ukimanuka@gmail.com>
2023-12-06 14:41:56 +0300

    understanding the controlled forms

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import DisplayData from "./DisplayData";
+
+function ParentComponent() {
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+
+  function handleFirstNameChange(event) {
+    console.log(event.target.value);
+    setFirstName(event.target.value);
+    
+  }
+
+  function handleLastNameChange(event) {
+    console.log(event.target.value);
+    setLastName(event.target.value);
+  }
+
+  return (
+    <div>
+    <Form
+      firstName={firstName}
+      lastName={lastName}
+      handleFirstNameChange={handleFirstNameChange}
+      handleLastNameChange={handleLastNameChange}
+    />
+    <DisplayData firstName={firstName} lastName={lastName} />
+    </div>
+  );
+}
+
+export default ParentComponent;
